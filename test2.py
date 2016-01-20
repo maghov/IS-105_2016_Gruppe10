87bc87405f995bde75c4626005e965a157761bb0
eiriho15 <EirikOliversen@eiriks-mbp.uia.no>
2016-01-20 13:20:55 +0100

    3

---
+++ test2.py
@@ -0,0 +1,22 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+hehehehe
+
+magnus
+
+mohammad
+
+
+efrit
+
+lol
+
+john
+
+eirik
+
+nissen
+=======
+>>>>>>> origin/master
+>>>>>>> Stashed changes
