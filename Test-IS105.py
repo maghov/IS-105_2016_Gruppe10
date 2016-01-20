63213160e5d109ba115c5899f6f0b1dcb1adf352
Magnus Høvik <maghovk@gmail.com>
2016-01-20 14:34:21 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	test2.py

--- Test-IS105.py
+++ Test-IS105.py
@@ -1,1 +1,9 @@
-Magnus
+#Program for å gange to tall
+    
+    print('Program for å gange to tall')
+    Tall1 = int(input('Skriv inn tall nr.1 = '))
+    Tall2 = int(input('Skriv inn tall nr.2 = '))
+    
+    Sum = Tall1*Tall2
+    
+    print('Sum av tall nr.1 og tall nr.2 =',Sum,)
