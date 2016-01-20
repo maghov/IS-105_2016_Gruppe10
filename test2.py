f777836239cf89e5f2009a6ed08332a793110d99
johnjnsen <Johnjnsen@johns-mbp.uia.no>
2016-01-20 14:05:37 +0100

    Py i navn
    adda .py på slutten av navnet

---
+++ test2.py
@@ -0,0 +1,24 @@
+John
+
+
+Mohammad
+
+Eirik 4 president
+
+
+Magnus H
+
+
+eirik
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+
+Magnus tetter en do akkurat nå
+=======
+ 
+Erik
+>>>>>>> origin/master
+=======
+ 
+>>>>>>> parent of 15f5915... LOL
