69eee022cef0ef7579483d39beaeda4f174d6d27
John Jensen <johnjnsen@gmail.com>
2016-02-25 17:51:48 +0100

    AddetListe

--- ICA05/ICA05b.py
+++ ICA05/ICA05b.py
@@ -1,25 +1,28 @@
 import timeit
-
-haystack = open('test.txt', 'r+').read()
-needle = ''
-
+ 
+haystack = open('test.txt', 'r+')
+words = list(haystack.read().split())
+needle = 'pollen into flowers'
+ 
 def search_fast(haystack, needle):
     for item in haystack:
+        if item == needle:
+            return_value = True        
         return True
     return False
-
-
-def search_slow(haystack, needle): 
+ 
+ 
+def search_slow(haystack, needle):
     return_value = False
     for item in haystack:
         if item == needle:
             return_value = True
     return return_value
-
-
-
+ 
+ 
+ 
 search_slow(haystack, needle)
 print(timeit.timeit("search_slow(haystack, needle)", setup="from __main__ import search_slow, haystack, needle"))
-
+ 
 search_fast(haystack, needle)
 print(timeit.timeit("search_fast(haystack, needle)", setup="from __main__ import search_fast, haystack, needle"))
