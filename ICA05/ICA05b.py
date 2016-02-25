f87b12f6d175f4b785c1f9571d82c23881cdbc9f
Mohammad Hussain <mo.huss2@gmail.com>
2016-02-25 12:34:49 +0100

    lol

--- ICA05/ICA05b.py
+++ ICA05/ICA05b.py
@@ -1,23 +1,26 @@
-def search_fast(haystack, needle): 
+
+haystack = open('test.txt', 'r+')
+needle = 'test'
+
+def search_fast(haystack, needle):
     for item in haystack:
-        if item == needle: 
-            return True
-        return False
+        return True
+    return False
 
 
 def search_slow(haystack, needle): 
     return_value = False
     for item in haystack:
-        if item == needle: 
+        if item == needle:
             return_value = True
+            print needle
     return return_value
 
-#Liste
-haystack = ['def-456', 'ghi-789', 'abc-123', 'jwjdkoa', 'jiwdjwoa', 'j9wadwaa', 'j9qid9']
-
-#Det man soker etter
-needle = 'def-456'
 
 #Starter sok
 import timeit
+search_slow(haystack, needle)
+print(timeit.timeit("search_slow(haystack, needle)", setup="from __main__ import search_slow, haystack, needle"))
+
+search_fast(haystack, needle)
 print(timeit.timeit("search_fast(haystack, needle)", setup="from __main__ import search_fast, haystack, needle"))
