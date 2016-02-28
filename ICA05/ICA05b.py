99737a0398f66b836243b7f12a5d698990d50bd6
Magnus <maghovk@gmail.com>
2016-02-28 21:35:30 +0100

    Update oppgave B

--- ICA05/ICA05b.py
+++ ICA05/ICA05b.py
@@ -1,26 +1,36 @@
-
-haystack = open('test2.txt', 'r+')
-needle = 'test'
-
+import timeit
+ 
+  
 def search_fast(haystack, needle):
     for item in haystack:
-        return True
+        if item == needle:
+        	return True
     return False
-
-
-def search_slow(haystack, needle): 
+ 
+ 
+def search_slow(haystack, needle):
     return_value = False
     for item in haystack:
         if item == needle:
             return_value = True
-            print needle
     return return_value
-
-
-#Starter sok
-import timeit
-search_slow(haystack, needle)
-print(timeit.timeit("search_slow(haystack, needle)", setup="from __main__ import search_slow, haystack, needle"))
-
-search_fast(haystack, needle)
-print(timeit.timeit("search_fast(haystack, needle)", setup="from __main__ import search_fast, haystack, needle"))
+    
+    
+def test_slow(): 
+	int = range(1000000) 
+	search_slow(int, 900000)
+	
+	
+	"""liste = []
+	for i in range(500):
+		liste.append(i)
+		chr(i)"""
+	
+	
+def test_fast(): 
+	int = range(1000000)
+	search_fast(int, 900000)
+	
+	 
+print(timeit.timeit("test_slow()", setup="from __main__ import test_slow", number = 10))
+print(timeit.timeit("test_fast()", setup="from __main__ import test_fast", number = 10))
