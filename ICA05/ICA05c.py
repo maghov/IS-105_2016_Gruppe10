042743c804ee6f4100a9e319e6f9becd6a4ab4e3
John Jensen <johnjnsen@gmail.com>
2016-02-28 20:56:40 +0100

    Ny_Test

--- ICA05/ICA05c.py
+++ ICA05/ICA05c.py
@@ -1,27 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 4
-slow_tid = (21, 35, 30, 35)
-slow_slow = (0, 0, 0, 0)
+N = 3
+slow_tid = (0.00692105293274, 0.00519990921021, 0.0650689601898)
+#slow_slow = (0, 0, 0, 0)
 
 ind = np.arange(N)  # the x locations for the groups
 width = 0.25       # the width of the bars
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(ind, slow_tid, width, color='r',yerr=slow_tid)
+rects1 = ax.bar(ind, slow_tid, width, color='r')
 
-fast_tid = (25, 32, 34, 20)
+fast_tid = (0.00692105293274, 0.00393009185791, 0.0226318836212)
 #fast_fast = (3, 5, 2, 3)
 rects2 = ax.bar(ind + width, fast_tid, width, color='b')
 
 # add some text for labels, title and axes ticks
-ax.set_ylabel('Tid')
+ax.set_ylabel('Tid i MS - TimeIt = 10')
 ax.set_title('Slow og Fast algorittmer')
 ax.set_xticks(ind + width)
-ax.set_xticklabels(('Haystack = 1k \n Needle = 100', 'Haystack = 10k \n Needle = 1k', 'Haystack = 100k \n Needle = 10k', "Haystack = 1 Mill \n Needle = 100k"))
+ax.set_xticklabels(('Haystack = 1k \n Needle = 100', 'Haystack = 10k \n Needle = 1k', 'Haystack = 100k \n Needle = 10k'))
 
-ax.legend((rects1[0], rects2[0]), ('Slow', 'Fast'))
+ax.legend((rects1[0], rects2[0]), ('Slow', 'Fast' 'h=haystack'))
 
 
 def autolabel(rects):
@@ -33,6 +33,6 @@ def autolabel(rects):
                 ha='center', va='bottom')
 
 autolabel(rects1)
-autolabel(rects2)
+autolabel(rects2) 
 
 plt.show()
