93af3420bc0f212edf88b47e0e55683396b3b0c4
johnjnsen <johnjnsen@gmail.com>
2016-02-28 14:35:35 +0100

    Fikset noe spelling errors

--- ICA05/ICA05c.py
+++ ICA05/ICA05c.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 N = 4
-slow_tid = (0.21, 0.35, 0.30, 0.35)
+slow_tid = (21, 35, 30, 35)
 slow_slow = (0, 0, 0, 0)
 
 ind = np.arange(N)  # the x locations for the groups
@@ -11,7 +11,7 @@ width = 0.25       # the width of the bars
 fig, ax = plt.subplots()
 rects1 = ax.bar(ind, slow_tid, width, color='r',yerr=slow_tid)
 
-fast_tid = (0.25, 0.32, 0.34, 0.20)
+fast_tid = (25, 32, 34, 20)
 #fast_fast = (3, 5, 2, 3)
 rects2 = ax.bar(ind + width, fast_tid, width, color='b')
 
