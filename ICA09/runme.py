82ce6b4ae0b7da7fcf074090d3deca12e24f3bdc
eiriho15 <EirikOliversen@annes-air-2.uia.no>
2016-05-31 13:59:45 +0200

    eirik

---
+++ ICA09/runme.py
@@ -0,0 +1,12 @@
+from sm import *
+from river import *
+from interaction import *
+
+
+#This script acts as the "front" for easy testing and running. All you have to do is run this to activate the main loop.
+
+'''Hierarcy:'''
+#interaction.py
+#river.py
+#sm.py
+    
