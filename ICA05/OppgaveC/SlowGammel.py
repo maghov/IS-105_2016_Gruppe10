22d9141e9f27b2d2d3235a79f5a0baea08d093d0
Magnus Hovik <maghovk@gmail.com>
2016-06-01 14:08:20 +0200

    magnus

--- ICA05/OppgaveC/SlowGammel.py
+++ ICA05/OppgaveC/SlowGammel.py
@@ -4,11 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 N = 4 # antall grafer
-slow_tid_needle100k = (65.28, 71.89, 43.54, 44.62)
+slow_tid_needle100k = (65.28, 71.89, 43.54, 44.62) # Setter verdiene til grafen.
 
 
 ind = np.arange(N)
-width = 0.35
+width = 0.35 # setter bredden på grafen.
 
 fig, ax = plt.subplots()
 rects1 = ax.bar(ind, slow_tid_needle100k, width, color='r')
@@ -17,16 +17,15 @@ slow_tid_needle900k = (60.88, 71.31, 41.27, 43.98)
 
 rects2 = ax.bar(ind + width, slow_tid_needle900k, width, color='b')
 
-ax.set_ylabel('Tid i sekunder * 100')
-ax.set_title('Search_Slow Algoritme - Haystack = 1 million')
+ax.set_ylabel('Tid i sekunder * 100') # navngir Y aksen
+ax.set_title('Search_Slow Algoritme - Haystack = 1 million') # navngir tittelen
 ax.set_xticks(ind + width)
-ax.set_xticklabels(('Magnus', 'Erik', 'Mohammad/John/Eirik', 'Per'))
+ax.set_xticklabels(('Magnus', 'Erik', 'Mohammad/John/Eirik', 'Per')) #navngir hver graf.
 
 ax.legend((rects1[0], rects2[0]), ('Needle=100k', 'Needle=900k'))
 
 
-def autolabel(rects):
-    # attach some text labels
+def autolabel(rects): #metode som lager grafene. 
     for rect in rects:
         height = rect.get_height()
         ax.text(rect.get_x() + rect.get_width()/2., 5.05*height,
